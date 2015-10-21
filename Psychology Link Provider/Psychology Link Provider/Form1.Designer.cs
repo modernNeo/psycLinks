42c56cbaf22a689dd8954fa8b4e9953f3eb908e5
modernNeo <peyvand.manshadi@gmail.com>
2015-10-21 16:15:58 -0700

    initial commit with all files

---
+++ Psychology Link Provider/Psychology Link Provider/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿using System.Windows.Forms;
+
+namespace Psychology_Link_Provider
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBoxID = new System.Windows.Forms.TextBox();
+            this.txtBoxDef = new System.Windows.Forms.TextBox();
+            this.txtBoxMSCS = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.labelDefinition = new System.Windows.Forms.Label();
+            this.labelMSCS = new System.Windows.Forms.Label();
+            this.labelLabID = new System.Windows.Forms.Label();
+            this.buttonFirstLink = new System.Windows.Forms.Button();
+            this.buttonSecondLink = new System.Windows.Forms.Button();
+            this.buttonThirdLink = new System.Windows.Forms.Button();
+            this.buttonFourthLink = new System.Windows.Forms.Button();
+            this.buttonFifthLink = new System.Windows.Forms.Button();
+            this.labelErrors = new System.Windows.Forms.Label();
+            this.labelID = new System.Windows.Forms.TextBox();
+            this.doneButton = new System.Windows.Forms.Button();
+            this.buttonDebriefing = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtBoxID
+            // 
+            this.txtBoxID.Location = new System.Drawing.Point(59, 81);
+            this.txtBoxID.Name = "txtBoxID";
+            this.txtBoxID.Size = new System.Drawing.Size(259, 20);
+            this.txtBoxID.TabIndex = 3;
+            // 
+            // txtBoxDef
+            // 
+            this.txtBoxDef.Location = new System.Drawing.Point(76, 55);
+            this.txtBoxDef.Name = "txtBoxDef";
+            this.txtBoxDef.Size = new System.Drawing.Size(77, 20);
+            this.txtBoxDef.TabIndex = 1;
+            // 
+            // txtBoxMSCS
+            // 
+            this.txtBoxMSCS.Location = new System.Drawing.Point(203, 55);
+            this.txtBoxMSCS.Name = "txtBoxMSCS";
+            this.txtBoxMSCS.Size = new System.Drawing.Size(115, 20);
+            this.txtBoxMSCS.TabIndex = 2;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(10, 107);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(308, 23);
+            this.btnSubmit.TabIndex = 4;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.buttonSubmit_Click);
+            // 
+            // labelDefinition
+            // 
+            this.labelDefinition.AutoSize = true;
+            this.labelDefinition.Location = new System.Drawing.Point(19, 59);
+            this.labelDefinition.Name = "labelDefinition";
+            this.labelDefinition.Size = new System.Drawing.Size(51, 13);
+            this.labelDefinition.TabIndex = 4;
+            this.labelDefinition.Text = "Definition";
+            // 
+            // labelMSCS
+            // 
+            this.labelMSCS.AutoSize = true;
+            this.labelMSCS.Location = new System.Drawing.Point(160, 58);
+            this.labelMSCS.Name = "labelMSCS";
+            this.labelMSCS.Size = new System.Drawing.Size(37, 13);
+            this.labelMSCS.TabIndex = 5;
+            this.labelMSCS.Text = "MSCS";
+            // 
+            // labelLabID
+            // 
+            this.labelLabID.AutoSize = true;
+            this.labelLabID.Location = new System.Drawing.Point(16, 84);
+            this.labelLabID.Name = "labelLabID";
+            this.labelLabID.Size = new System.Drawing.Size(41, 13);
+            this.labelLabID.TabIndex = 6;
+            this.labelLabID.Text = "LAB ID";
+            // 
+            // buttonFirstLink
+            // 
+            this.buttonFirstLink.Location = new System.Drawing.Point(10, 167);
+            this.buttonFirstLink.Name = "buttonFirstLink";
+            this.buttonFirstLink.Size = new System.Drawing.Size(143, 23);
+            this.buttonFirstLink.TabIndex = 7;
+            this.buttonFirstLink.Text = "First Link - Consent";
+            this.buttonFirstLink.UseVisualStyleBackColor = true;
+            this.buttonFirstLink.Visible = false;
+            this.buttonFirstLink.Click += new System.EventHandler(this.buttonFirstLink_Click);
+            // 
+            // buttonSecondLink
+            // 
+            this.buttonSecondLink.Location = new System.Drawing.Point(162, 169);
+            this.buttonSecondLink.Name = "buttonSecondLink";
+            this.buttonSecondLink.Size = new System.Drawing.Size(156, 21);
+            this.buttonSecondLink.TabIndex = 8;
+            this.buttonSecondLink.UseVisualStyleBackColor = true;
+            this.buttonSecondLink.Visible = false;
+            this.buttonSecondLink.Click += new System.EventHandler(this.buttonSecondLink_Click);
+            // 
+            // buttonThirdLink
+            // 
+            this.buttonThirdLink.Location = new System.Drawing.Point(10, 194);
+            this.buttonThirdLink.Name = "buttonThirdLink";
+            this.buttonThirdLink.Size = new System.Drawing.Size(143, 23);
+            this.buttonThirdLink.TabIndex = 9;
+            this.buttonThirdLink.UseVisualStyleBackColor = true;
+            this.buttonThirdLink.Visible = false;
+            this.buttonThirdLink.Click += new System.EventHandler(this.buttonThirdLink_Click);
+            // 
+            // buttonFourthLink
+            // 
+            this.buttonFourthLink.Location = new System.Drawing.Point(162, 194);
+            this.buttonFourthLink.Name = "buttonFourthLink";
+            this.buttonFourthLink.Size = new System.Drawing.Size(156, 23);
+            this.buttonFourthLink.TabIndex = 10;
+            this.buttonFourthLink.Text = "Fourth Link - Direct Questions";
+            this.buttonFourthLink.UseVisualStyleBackColor = true;
+            this.buttonFourthLink.Visible = false;
+            this.buttonFourthLink.Click += new System.EventHandler(this.buttonFourthLink_Click);
+            // 
+            // buttonFifthLink
+            // 
+            this.buttonFifthLink.Location = new System.Drawing.Point(10, 223);
+            this.buttonFifthLink.Name = "buttonFifthLink";
+            this.buttonFifthLink.Size = new System.Drawing.Size(143, 23);
+            this.buttonFifthLink.TabIndex = 11;
+            this.buttonFifthLink.Text = "Fifth Link - Demographics";
+            this.buttonFifthLink.UseVisualStyleBackColor = true;
+            this.buttonFifthLink.Visible = false;
+            this.buttonFifthLink.Click += new System.EventHandler(this.buttonFifthLink_Click);
+            // 
+            // labelErrors
+            // 
+            this.labelErrors.AutoSize = true;
+            this.labelErrors.Location = new System.Drawing.Point(13, 13);
+            this.labelErrors.Name = "labelErrors";
+            this.labelErrors.Size = new System.Drawing.Size(0, 13);
+            this.labelErrors.TabIndex = 13;
+            // 
+            // labelID
+            // 
+            this.labelID.BackColor = this.BackColor;
+            this.labelID.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.labelID.Location = new System.Drawing.Point(10, 150);
+            this.labelID.Name = "labelID";
+            this.labelID.ReadOnly = true;
+            this.labelID.Size = new System.Drawing.Size(308, 13);
+            this.labelID.TabIndex = 14;
+            this.labelID.TabStop = false;
+            this.labelID.Visible = false;
+            // 
+            // doneButton
+            // 
+            this.doneButton.Location = new System.Drawing.Point(10, 253);
+            this.doneButton.Name = "doneButton";
+            this.doneButton.Size = new System.Drawing.Size(308, 23);
+            this.doneButton.TabIndex = 15;
+            this.doneButton.Text = "Done";
+            this.doneButton.UseVisualStyleBackColor = true;
+            this.doneButton.Visible = false;
+            this.doneButton.Click += new System.EventHandler(this.doneButton_Click);
+            // 
+            // buttonDebriefing
+            // 
+            this.buttonDebriefing.Location = new System.Drawing.Point(160, 224);
+            this.buttonDebriefing.Name = "buttonDebriefing";
+            this.buttonDebriefing.Size = new System.Drawing.Size(158, 23);
+            this.buttonDebriefing.TabIndex = 16;
+            this.buttonDebriefing.Text = "Debriefing";
+            this.buttonDebriefing.UseVisualStyleBackColor = true;
+            this.buttonDebriefing.Visible = false;
+            this.buttonDebriefing.Click += new System.EventHandler(this.buttonDebriefing_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 290);
+            this.Controls.Add(this.buttonDebriefing);
+            this.Controls.Add(this.doneButton);
+            this.Controls.Add(this.labelID);
+            this.Controls.Add(this.labelErrors);
+            this.Controls.Add(this.buttonFifthLink);
+            this.Controls.Add(this.buttonFourthLink);
+            this.Controls.Add(this.buttonThirdLink);
+            this.Controls.Add(this.buttonSecondLink);
+            this.Controls.Add(this.buttonFirstLink);
+            this.Controls.Add(this.labelLabID);
+            this.Controls.Add(this.labelMSCS);
+            this.Controls.Add(this.labelDefinition);
+            this.Controls.Add(this.txtBoxMSCS);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtBoxDef);
+            this.Controls.Add(this.txtBoxID);
+            this.Name = "Form1";
+            this.Text = "Psychology Link Provider";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBoxID;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.TextBox txtBoxMSCS;
+        private System.Windows.Forms.Label labelDefinition;
+        private System.Windows.Forms.Label labelMSCS;
+        private System.Windows.Forms.Label labelLabID;
+        private TextBox txtBoxDef;
+        private Button buttonFirstLink;
+        private Button buttonSecondLink;
+        private Button buttonThirdLink;
+        private Button buttonFourthLink;
+        private Button buttonFifthLink;
+        private Label labelErrors;
+        private TextBox labelID;
+        private Button doneButton;
+        private Button buttonDebriefing;
+    }
+}
+
---
+++ Psychology Link Provider/Psychology Link Provider/Form1.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Psychology_Link_Provider
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[] URL = new string[6];
+        int buttonsClicked = 0;
+        int Def, MSCS;
+        int currentPage = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //for confidentality concerns, all links redirect to the git repository to prevent any abuse of the links used for actual study
+            URL[0] = "https://github.com/modernNeo/psycLinks"; 
+            URL[1] = "https://github.com/modernNeo/psycLinks"; 
+            URL[2] = "https://github.com/modernNeo/psycLinks";
+            URL[3] = "https://github.com/modernNeo/psycLinks"; 
+            URL[4] = "https://github.com/modernNeo/psycLinks";
+            URL[5] = "https://github.com/modernNeo/psycLinks";
+        }
+
+        private void buttonSubmit_Click(object sender, EventArgs e)
+        {
+            bool passed = true;
+            labelErrors.Text = "";
+            int garbage;
+            labelID.Text = txtBoxID.Text;
+            if (!int.TryParse(txtBoxMSCS.Text, out MSCS) || MSCS < 1 || MSCS > 2)
+            {
+                passed = false;
+                labelErrors.Text = "Incorrect Input for MSCS\n";
+
+            }
+
+            if (!int.TryParse(txtBoxDef.Text, out Def) || Def < 1 || Def > 2)
+            {
+                passed = false;
+                labelErrors.Text += "Incorrect Input for Def";
+            }
+            if (txtBoxID.Text.Length != 17)
+            {
+                passed = false;
+                labelErrors.Text += "Incorrect Input for Lab ID";
+                System.Diagnostics.Debug.WriteLine(1);
+
+            }
+            else
+            {
+                for (int i = 0; i < 17; i++)
+                {
+                    if ((i < 4 && i > -1) || (i == 5 || i == 6) || (i == 8 || i == 9) || (i > 10))
+                    {
+                        if (!int.TryParse(txtBoxID.Text[i].ToString(), out garbage))
+                        {
+                            passed = false;
+                            labelErrors.Text += "Incorrect Input for Lab ID";
+                            System.Diagnostics.Debug.WriteLine("2" + i);
+
+                        }
+                    }
+                    else
+                    {
+                        if (txtBoxID.Text[i].ToString() != ".")
+                        {
+                            passed = false;
+                            labelErrors.Text += "Incorrect Input for Lab ID";
+                            System.Diagnostics.Debug.WriteLine(3);
+
+                        }
+                    }
+                }
+            }
+            if (passed == true)
+            {
+                if (MSCS == 1)
+                {
+                    buttonSecondLink.Text = "Second Link - MSCS";
+                    buttonThirdLink.Text = "Third Link - What is SC?";
+                }
+                else
+                {
+                    buttonSecondLink.Text = "Second Link - What is SC?";
+                    buttonThirdLink.Text = "Third Link - MSCS";
+                }
+                txtBoxID.Visible = false;
+                txtBoxDef.Visible = false;
+                txtBoxMSCS.Visible = false;
+                btnSubmit.Visible = false;
+                labelDefinition.Visible = false;
+                labelMSCS.Visible = false;
+                labelLabID.Visible = false;
+
+                buttonFirstLink.Visible = true;
+                buttonSecondLink.Visible = true;
+                buttonThirdLink.Visible = true;
+                buttonFourthLink.Visible = true;
+                buttonFifthLink.Visible = true;
+                labelID.Visible = true;
+                doneButton.Visible = true;
+                doneButton.Enabled = false;
+                buttonDebriefing.Visible = true;
+            }
+        }
+
+        private void buttonFirstLink_Click(object sender, EventArgs e)
+        {
+            buttonsClicked++;
+            System.Diagnostics.Process.Start(URL[currentPage]);
+            buttonFirstLink.Enabled = false;
+            if (buttonsClicked == 6)
+            {
+                doneButton.Enabled = true;
+            }
+        }
+
+        private void buttonSecondLink_Click(object sender, EventArgs e)
+        {
+            buttonsClicked++;
+            if (MSCS == 1)
+            {
+                currentPage = 3;
+            }
+            else
+            {
+                if (Def == 1)
+                {
+                    currentPage = 1;
+                }
+                else
+                {
+                    currentPage = 2;
+                }
+            }
+            System.Diagnostics.Process.Start(URL[currentPage]);
+            buttonSecondLink.Enabled = false;
+            if (buttonsClicked == 6)
+            {
+                doneButton.Enabled = true;
+            }
+        }
+
+        private void buttonThirdLink_Click(object sender, EventArgs e)
+        {
+            buttonsClicked++;
+            if (MSCS == 2)
+            {
+                currentPage = 3;
+            }
+            else
+            {
+                if (Def == 1)
+                {
+                    currentPage = 1;
+                }
+                else
+                {
+                    currentPage = 2;
+                }
+            }
+            System.Diagnostics.Process.Start(URL[currentPage]);
+            currentPage=4;
+            buttonThirdLink.Enabled = false;
+            if (buttonsClicked == 6)
+            {
+                doneButton.Enabled = true;
+            }
+        }
+
+        private void buttonFourthLink_Click(object sender, EventArgs e)
+        {
+            buttonsClicked++;
+            System.Diagnostics.Process.Start(URL[currentPage]);
+            currentPage++;
+            buttonFourthLink.Enabled = false;
+            if (buttonsClicked == 6)
+            {
+                doneButton.Enabled = true;
+            }
+        }
+
+        private void buttonFifthLink_Click(object sender, EventArgs e)
+        {
+            buttonsClicked++;
+            System.Diagnostics.Process.Start(URL[currentPage]);
+            currentPage++;
+            buttonFifthLink.Enabled = false;
+            if (buttonsClicked == 6)
+            {
+                doneButton.Enabled = true;
+            }
+        }
+
+        private void buttonDebriefing_Click(object sender, EventArgs e)
+        {
+            buttonsClicked++;
+            buttonDebriefing.Enabled = false;
+            if (buttonsClicked == 6)
+            {
+                doneButton.Enabled = true;
+            }
+
+        }
+
+        private void doneButton_Click(object sender, EventArgs e)
+        {
+            // Specify a name for your top-level folder. 
+            string pwd = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            string pathString = System.IO.Path.Combine(pwd, "MSCSFolder");
+
+
+            String filePath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/MSCSFolder/";
+
+            String fileName = "file.csv";
+
+            string header = "Lab ID,Consent,Definition (1=N 2=Y), 1=MSCS 1st 2=MSCS 2nd, Direct questions, Demographics, Debriefing" + Environment.NewLine;
+
+
+
+            if (!File.Exists(filePath + fileName))
+            {
+                System.IO.Directory.CreateDirectory(pathString);
+                System.IO.File.WriteAllText(filePath + fileName, header);
+
+                //create folder
+                //add header to file
+            }
+            string appendText = labelID.Text + ",X," + Def + "," + MSCS + ",X,X,X" + Environment.NewLine;
+            File.AppendAllText(filePath + fileName, appendText);
+            labelErrors.Text = "Output saved to: " + filePath + fileName;
+            txtBoxID.Text = "";
+            txtBoxMSCS.Text = "";
+            txtBoxDef.Text = "";
+            txtBoxID.Visible = true;
+            txtBoxDef.Visible = true;
+            txtBoxMSCS.Visible = true;
+            btnSubmit.Visible = true;
+            labelDefinition.Visible = true;
+            labelMSCS.Visible = true;
+            labelLabID.Visible = true;
+
+            buttonDebriefing.Visible = false;
+            buttonFirstLink.Visible = false;
+            buttonSecondLink.Visible = false;
+            buttonThirdLink.Visible = false;
+            buttonFourthLink.Visible = false;
+            buttonFifthLink.Visible = false;
+            labelID.Visible = false;
+            doneButton.Visible = false;
+
+        }
+
+    }
+}
